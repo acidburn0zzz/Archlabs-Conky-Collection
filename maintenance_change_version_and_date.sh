d6ca6d33cad7bf16ac0f56e867d4e655d571d885
Erik Dubois <erik.dubois@gmail.com>
2017-07-06 15:32:09 +0200

    version up

--- maintenance_change_version_and_date.sh
+++ maintenance_change_version_and_date.sh
@@ -8,5 +8,5 @@
 #
 ##################################################################################################################
 
-find .  -name "*.conkyrc" -type f -exec sed -i  	's/v2.0.2/v2.1.0/g' {} \;
-find .  -name "*.conkyrc" -type f -exec sed -i  	's/02\/07\/2017/06\/07\/2017/g' {} \;
+find .  -name "*.conkyrc" -type f -exec sed -i  	's/v2.1.0/v2.1.1/g' {} \;
+find .  -name "*.conkyrc" -type f -exec sed -i  	's/06\/07\/2017/06\/07\/2017/g' {} \;
